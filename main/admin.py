ad72c11c78d97ded823bd4f059db16ab2a9bef55
Atorami <atoramiseo@mail.ru>
2023-12-11 21:31:28 +0100

    Fix : fix category name display at admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Post, Category
 
-# admin.site.register(Post)
-admin.site.register(Category)
-
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'category', 'pub_date']
     list_filter = ['author', 'category', 'pub_date']
     search_fields = ['title', 'body']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
