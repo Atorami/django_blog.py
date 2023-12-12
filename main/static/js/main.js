1ed449e9b087b468a250b3e4798aaf7cb32944d2
Atorami <atoramiseo@mail.ru>
2023-12-12 21:09:18 +0100

    Feat : add snowfall to promo background

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -3,4 +3,30 @@ document.addEventListener('DOMContentLoaded', function (event) {
         if(input_data === 'None'){
             document.getElementById('searchbar').value = ''
         }
+    const snow_background = document.querySelector('.weather_wrapper')
+
+const startSnowing = () =>{
+    const snow = document.createElement('span')
+    snow.className = 'snow'
+
+    const minSize = 5;
+    const maxSize = 10;
+
+
+    let snowSize = Math.random()*(maxSize-minSize) + minSize
+
+    snow.style.width = snowSize + 'px'
+    snow.style.height = snowSize + 'px'
+
+    snow.style.left = Math.random()*100+'%'
+
+    snow_background.appendChild(snow)
+
+    setTimeout(() =>{
+        snow.remove()
+    }, 1400)
+}
+
+setInterval(startSnowing, 100)
 })
+
