989723147dcdca873f9b772eda166e4a1c8806dc
Atorami <atoramiseo@mail.ru>
2023-12-12 08:57:39 +0100

    Feat : add comments displaying in profile

--- main/models.py
+++ main/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
